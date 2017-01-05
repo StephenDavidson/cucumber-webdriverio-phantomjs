4c003d00515b5aae6331269353e7eaf3cb925a3c
Stephen Davidson <shdavidson90@gmail.com>
2017-01-05 10:33:45 +0100

    update to cucumber rc5

--- features/step-definitions/search.js
+++ features/step-definitions/search.js
@@ -1,9 +1,11 @@
-module.exports = function searchSteps() {
-  this.Given(/^I am on the search page$/, () => browser.url('https://duckduckgo.com'));
+const { defineSupportCode } = require('cucumber');
 
-  this.When(/^I search for "(.*)"$/, value => browser.setValue('input[name="q"]', value)
+defineSupportCode(({ Given, When, Then }) => {
+  Given(/^I am on the search page$/, () => browser.url('https://duckduckgo.com'));
+
+  When(/^I search for "(.*)"$/, value => browser.setValue('input[name="q"]', value)
                   .click('input[type="submit"]'));
 
-  this.Then(/^I should see "(.*)" in the search results$/, value => browser.waitForVisible('#r1-0')
+  Then(/^I should see "(.*)" in the search results$/, value => browser.waitForVisible('#r1-0')
                   .getText('#r1-0').then(text => expect(text, 'to contain', value)));
-};
+});
--- features/support/env.js
+++ features/support/env.js
@@ -1,23 +1,24 @@
+const { defineSupportCode } = require('cucumber');
 const expect = require('unexpected');
 const phantomjs = require('phantomjs-prebuilt');
 const WebDriverIO = require('webdriverio');
 
 const wdOpts = { desiredCapabilities: { browserName: 'phantomjs' } };
 const browser = WebDriverIO.remote({ wdOpts });
+let process;
 
 global.browser = browser;
 global.expect = expect;
 
-module.exports = function hooks() {
-  this.registerHandler('BeforeFeatures', (event, done) => {
+defineSupportCode(({ registerHandler }) => {
+  registerHandler('BeforeFeatures', (features, done) => {
     phantomjs.run('--webdriver=4444').then((program) => {
-      this.process = program;
+      process = program;
       browser.init().setViewportSize({ width: 1600, height: 800 }).call(done);
     });
   });
 
-  this.registerHandler('AfterFeatures', (event, done) => {
-    if (this.process) this.process.kill();
-    done();
+  registerHandler('AfterFeatures', () => {
+    if (process) process.kill();
   });
-};
+});
